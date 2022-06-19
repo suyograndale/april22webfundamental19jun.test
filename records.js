381f87965bcf04fcf84bc4c4b10055510599ce09
suyograndale <78803822+suyograndale@users.noreply.github.com>
2022-06-19 16:36:08 +0545

    Add files via upload

---
+++ records.js
@@ -0,0 +1,41 @@
+let friends = {
+    fname: 'Rachal', lname:"Green" ,id: 1, marks : 98 
+}
+
+console.log(friends.fname, friends.lname);
+
+
+let friends1 =
+{
+    fname: 'Ross', lname:"Geller" , id: 2, marks : 23 
+}
+
+console.log(friends1.fname, friends1.lname);
+
+
+let friends2 = 
+{fname: 'Monica ',lname:"Geller" , id: 3, marks : 75 }
+console.log(friends2.fname, friends2.lname);
+
+
+let friends3 =
+{
+    fname: 'Joey',lname:" Tribbiani" , id: 4, marks : 46
+}
+console.log(friends3.fname, friends3.lname);
+
+
+
+let friends4 =
+{fname: 'Chandler  ',lname:"Bing" , id: 5, marks : 78}
+
+console.log(friends4.fname, friends4.lname);
+
+
+let friends5 =
+{fname: 'Phoebe',lname:"Buffay" , id: 6, marks : 45}
+
+console.log(friends5.fname, friends5.lname);
+
+
+
